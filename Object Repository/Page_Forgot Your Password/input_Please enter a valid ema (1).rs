20e3103d6b6b8e875d7a1ff030764760d68f2aeb
electricsheep718 <nyybaseball718@gmail.com>
2019-01-03 14:57:57 -0500

    staged push

---
+++ Object Repository/Page_Forgot Your Password/input_Please enter a valid ema (1).rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Please enter a valid ema (1)</name>
+   <tag></tag>
+   <elementGuidId>a9ec26d2-8888-4ea5-bf2a-f73e3fa8f634</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='usertoForgotPassword_0']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>value</name>
+      <type>Main</type>
+      <value>Next</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>usertoForgotPassword_0</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-primary</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;usertoForgotPassword_0&quot;)</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <value>//input[@id='usertoForgotPassword_0']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <value>//form[@id='usertoForgotPassword']/div[5]/div[2]/input</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Please enter a valid email address.'])[1]/following::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='*'])[1]/following::input[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Back to login'])[1]/preceding::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=concat('id(', '&quot;', 'usertoForgotPassword_0', '&quot;', ')')])[1]/preceding::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//div[2]/input</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/ICRC External - Login - Forgot Password/Script1546543395155.groovy
@@ -0,0 +1,51 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://icrc-test.nci.nih.gov/')
+
+WebUI.click(findTestObject('Page_Introduction to Cancer Researc/a_Login (2) (1)'))
+
+WebUI.click(findTestObject('Page_Login/a_Forgot your password (1)'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Forgot Your Password/input__username (1)'), 'testuser1@nih.gov')
+
+WebUI.click(findTestObject('Object Repository/Page_Forgot Your Password/input__usertoForgotPassword_0 (1)'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Forgot Your Password/input__answer1'), 'nick')
+
+WebUI.setText(findTestObject('Object Repository/Page_Forgot Your Password/input__answer2'), 'first')
+
+WebUI.click(findTestObject('Object Repository/Page_Forgot Your Password/input__usertoSecurityQuestion_'))
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Enter a New Password/input__password'), 'iFGeFYmXIrWy72D5I5E8Dw==')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Enter a New Password/input__confirmPassword'), 'iFGeFYmXIrWy72D5I5E8Dw==')
+
+WebUI.click(findTestObject('Object Repository/Page_Enter a New Password/input__usertoResetPassword_0'))
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Enter a New Password/input__confirmPassword'), 'iFGeFYmXIrVXkUXHumJCLw==')
+
+WebUI.click(findTestObject('Object Repository/Page_Enter a New Password/input__usertoResetPassword_0'))
+
+WebUI.setText(findTestObject('Page_Login/input_Email Address_username (3)'), 'testuser1@nih.gov')
+
+WebUI.setEncryptedText(findTestObject('Page_Login/input_Password_password (3)'), 'iFGeFYmXIrVXkUXHumJCLw==')
+
+WebUI.click(findTestObject('Page_Login/input_Password_userlogin_0 (3)'))
+
+WebUI.click(findTestObject('Page_ICRC Application/a_Log out (2)'))
+
