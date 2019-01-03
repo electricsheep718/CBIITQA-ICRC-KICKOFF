804c8151bb981c1a85a7896c728d2032aae51186
electricsheep718 <nyybaseball718@gmail.com>
2019-01-03 15:11:44 -0500

    commit/push

---
+++ Object Repository/Page_Login/input_Password_btn btn-default (1).rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Password_btn btn-default (1)</name>
+   <tag></tag>
+   <elementGuidId>9bd8fa1b-9fde-406c-9a57-da6168291a58</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@value='CANCEL']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>value</name>
+      <type>Main</type>
+      <value>CANCEL</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>alt</name>
+      <type>Main</type>
+      <value>cancel</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-label</name>
+      <type>Main</type>
+      <value>false</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-default pull-right</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onclick</name>
+      <type>Main</type>
+      <value>window.location.href=&quot;http://icrc-test.nci.nih.gov&quot;</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;newboxes1&quot;)/div[@class=&quot;col-md-12&quot;]/div[@class=&quot;col-md-12&quot;]/input[@class=&quot;btn btn-default pull-right&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <value>//input[@value='CANCEL']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <value>//div[@id='newboxes1']/div[5]/div[3]/input</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Email Address:'])[1]/following::input[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Register now'])[1]/preceding::input[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//div[3]/input</value>
+   </webElementXpaths>
+</WebElementEntity>
--- Scripts/ICRC External - Smoke Test Verification/Script1546546081990.groovy
+++ Scripts/ICRC External - Smoke Test Verification/Script1546546081990.groovy
@@ -13,3 +13,77 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://icrc-test.nci.nih.gov/')
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Login (4)'))
+
+WebUI.click(findTestObject('Page_Login/input_Password_btn btn-default (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Register (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_Registration/input__usPersonType (1)'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Registration/select_--- Select a level --- (1)'), '163', 
+    true)
+
+WebUI.click(findTestObject('Object Repository/Page_Registration/a_CANCEL'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Application Information'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Apply Now'))
+
+WebUI.click(findTestObject('Object Repository/Page_Registration/input__usPersonType (1)'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Registration/select_--- Select a level --- (1)'), '163', 
+    true)
+
+WebUI.click(findTestObject('Object Repository/Page_Registration/a_CANCEL'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Login (4)'))
+
+WebUI.click(findTestObject('Page_Login/a_Forgot your password (2)'))
+
+WebUI.click(findTestObject('Page_Forgot Your Password/a_Back to login (2)'))
+
+WebUI.click(findTestObject('Page_Login/input_Password_btn btn-default (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Research Areas'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Center for Cancer Research ('))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Division of Cancer Epidemiol'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Center to Reduce Cancer Heal'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Division of Cancer Control a'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Division of Cancer Treatment'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Alumni'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/button_Full Bio'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/button_Full Bio_1'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Alumni Photo Gallery'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_FAQs'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/h3_Do I have to be a U.S. citi'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/h3_If I am graduating from col'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/h3_Can I submit a paper applic'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/h3_Can I submit a paper applic'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/h3_If I am graduating from col'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/h3_Do I have to be a U.S. citi'))
+
+WebUI.click(findTestObject('Object Repository/Page_Introduction to Cancer Researc/a_Contact Us'))
+
+WebUI.closeBrowser()
+
