860dfb0507c6f553052979b133055f4173633bd7
electricsheep718 <nyybaseball718@gmail.com>
2019-01-05 16:16:17 -0500

    asdad

--- Keywords/custom_javascript_click/javaclick.groovy
+++ Keywords/custom_javascript_click/javaclick.groovy
@@ -5,20 +5,20 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+
 import com.kms.katalon.core.annotation.Keyword
-import com.kms.katalon.core.checkpoint.Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling
-import com.kms.katalon.core.testcase.TestCase
-import com.kms.katalon.core.testdata.TestData
-import com.kms.katalon.core.testobject.TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webui.common.WebUiCommonHelper
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 import internal.GlobalVariable
 
-public class javaclick {def clickUsingJS(TestObject to, int timeout) {
+@javaclick
+def clickUsingJS(TestObject to, int timeout) {
 
 	try {
 		WebUI.click(to)
@@ -30,6 +30,4 @@ public class javaclick {def clickUsingJS(TestObject to, int timeout) {
 		executor.executeScript('arguments[0].click()', element)
 	}
 	throw(e)
-}
-
 }
