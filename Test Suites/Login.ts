65de1de0ec60508ace335706723473fd520bf041
electricsheep718 <nyybaseball718@gmail.com>
2019-01-05 22:00:13 -0500

    Successful Data Driven Login Script

---
+++ Test Suites/Login.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>046f73f3-44e4-4bf3-8707-833c27ef36ac</testSuiteGuid>
+   <testCaseLink>
+      <guid>752bf749-8818-4c74-b002-2d3d8d597fe3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ICRC External - Login Sample Data File</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>175c75be-d673-4fde-8019-19d433d2d364</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Test Data 1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>175c75be-d673-4fde-8019-19d433d2d364</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email address</value>
+         <variableId>3c697e63-05f6-47c7-9b17-a4bff0413025</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
