8a755e8a00d2f444daa235594ec28f42eabbccf3
electricsheep718 <nyybaseball718@gmail.com>
2019-01-15 11:01:42 -0500

    delete button commit

--- Object Repository/Page_ICRC Application/input_methoddeleteApplication.rs
+++ Object Repository/Page_ICRC Application/input_methoddeleteApplication.rs
@@ -1,22 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
-   <description>Delete Button</description>
-   <name>input_delete_button</name>
+   <description></description>
+   <name>input_methoddeleteApplication</name>
    <tag></tag>
-   <elementGuidId>3203f256-5935-493d-b02e-3d77bd5550a6</elementGuidId>
-   <selectorCollection>
-      <entry>
-         <key>XPATH</key>
-         <value>//input[@id='deleteApplication']</value>
-      </entry>
-      <entry>
-         <key>BASIC</key>
-         <value>//input[@type = 'submit' and @id = 'deleteApplication' and @name = 'method:deleteApplication']</value>
-      </entry>
-      <entry>
-         <key>CSS</key>
-      </entry>
-   </selectorCollection>
+   <elementGuidId>e454714b-bc14-42cf-9151-76c8eca1c0a7</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
@@ -69,18 +56,39 @@
       <value>id(&quot;applicationForm&quot;)/div[@class=&quot;col-md-12&quot;]/div[@class=&quot;col-md-12 text-right&quot;]/div[@class=&quot;button_wrapper&quot;]/input[@id=&quot;deleteApplication&quot;]</value>
    </webElementProperties>
    <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <value>//input[@id='deleteApplication']</value>
+   </webElementXpaths>
+   <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <value>//form[@id='applicationForm']/div[4]/div/div/input[5]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='ICRC-Contact@mail.nih.gov'])[1]/following::input[5]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='You can save a draft of your application and continue at a later time.'])[1]/following::input[5]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Application Deadline: Sunday, Mar 17, 2019.'])[1]/preceding::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//div/input[5]</value>
    </webElementXpaths>
 </WebElementEntity>
--- Scripts/Sami Test Cases/New Test Case/Script1547229638792.groovy
+++ Scripts/Sami Test Cases/New Test Case/Script1547229638792.groovy
@@ -13,9 +13,31 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.acceptAlert()
+WebUI.callTestCase(findTestCase('Sami Test Cases/ICRC External - Login'), [:], FailureHandling.STOP_ON_FAILURE)
 
-WebUI.acceptAlert()
+WebUI.maximizeWindow()
 
-WebUI.acceptAlert()
+WebUI.setText(findTestObject('Object Repository/Page_ICRC Application/input_Place of Employment_expA (1)'), 'John Hopkins University')
+
+WebUI.setText(findTestObject('Object Repository/Page_ICRC Application/input_Name of Supervisor_expAc (1)'), 'Liz Andreyev')
+
+WebUI.setText(findTestObject('Object Repository/Page_ICRC Application/input_Supervisor Phone _expAcc (1)'), '3012221092')
+
+WebUI.setText(findTestObject('Object Repository/Page_ICRC Application/input_Start Date_expAccStartDa (1)'), '01/01/2012')
+
+WebUI.setText(findTestObject('Object Repository/Page_ICRC Application/input_End Date_expAccEndDate (1)'), '01/01/2018')
+
+WebUI.setText(findTestObject('Object Repository/Page_ICRC Application/input_Hours Per Week_expAccHou (1)'), '40')
+
+WebUI.setText(findTestObject('Object Repository/Page_ICRC Application/textarea_Brief Summary of Duti (1)'), 'Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties Brief Summary of Duties ')
+
+WebUI.scrollToElement(findTestObject('Page_ICRC Application/a_Add_2 (1)'), 0)
+
+WebUI.click(findTestObject('Object Repository/Page_ICRC Application/a_Add_2 (1)'))
+
+WebUI.scrollToElement(findTestObject('Page_ICRC Application/input_ICRC-Contactmail.nih.gov'), 0)
+
+WebUI.click(findTestObject('Page_ICRC Application/input_ICRC-Contactmail.nih.gov'))
+
+WebUI.click(findTestObject('Object Repository/Page_ICRC Application/input__methodsaveApplication (1)'))
 
