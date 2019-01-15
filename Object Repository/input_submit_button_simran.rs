4d39cedfc525ba31a29b6414638d1b7080a33ff4
Simran Kaur <simran.kaur@nih.gov>
2019-01-15 16:33:40 -0500

    updates

---
+++ Object Repository/input_submit_button_simran.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Building a sample button</description>
+   <name>input_submit_button_simran</name>
+   <tag></tag>
+   <elementGuidId>af7e1b84-f5c8-48c5-8df4-e0651d483c7c</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
