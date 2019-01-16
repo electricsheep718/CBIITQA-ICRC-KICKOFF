73c8ac6e3f2c45fceadcca24b836d6a48841bbd3
electricsheep718 <nyybaseball718@gmail.com>
2019-01-16 14:34:05 -0500

    update to forgot password

---
+++ Test Suites/Forgot Password.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Forgot Password</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9c94d4e0-0bff-4f29-aff3-90b47383e523</testSuiteGuid>
+   <testCaseLink>
+      <guid>88239fcb-e45f-46d1-8f28-7f6f6ce8eaeb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sami Test Cases/ICRC External - Login - Forgot Password</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>68853b1e-8f87-4cca-8185-cd2554dd05cd</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/ForgotPassword Test Data</testDataId>
+      </testDataLink>
+   </testCaseLink>
+</TestSuiteEntity>
