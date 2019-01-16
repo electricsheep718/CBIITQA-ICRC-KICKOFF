81a2c82bd84d5c6f30adf7f7347090eeb347e403
electricsheep718 <nyybaseball718@gmail.com>
2019-01-16 16:39:07 -0500

    commit

--- Object Repository/Page_ICRC Application/button_Confirm_exitPopup.rs
+++ Object Repository/Page_ICRC Application/button_Confirm_exitPopup.rs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>input_Confirm_exitPopup</name>
+   <name>button_Confirm_exitPopup</name>
    <tag></tag>
    <elementGuidId>2ac1295a-e5c4-4001-962d-74b00e29f4f6</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
