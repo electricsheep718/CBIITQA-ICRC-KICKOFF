e38d2ab83d7900df809b87faf3dfafb6493817e1
electricsheep718 <nyybaseball718@gmail.com>
2019-01-17 00:24:31 -0500

    commit

---
+++ Test Suites/Test Suite Collections/Register and Submit Test Suites/Registrations Demo.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Registrations Demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ff9c7b00-a23a-40f9-ac00-e842c274cb79</testSuiteGuid>
+   <testCaseLink>
+      <guid>76d6cc74-34c4-4a4e-8d3d-bd60bbe51e4f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sami Test Cases/ICRC External - Registration</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>745b0b81-5368-45ea-bf5f-f37e0d9ef0c3</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Registration Sami</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>745b0b81-5368-45ea-bf5f-f37e0d9ef0c3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>FirstName</value>
+         <variableId>c708f2b5-e226-495e-9943-557a75d9de39</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>745b0b81-5368-45ea-bf5f-f37e0d9ef0c3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>LastName</value>
+         <variableId>26f712fc-67cb-4fdd-9281-acc1ace96cd2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>745b0b81-5368-45ea-bf5f-f37e0d9ef0c3</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>EmailAddress</value>
+         <variableId>383e42f1-4ab2-4aae-8e91-a81c082ee929</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
