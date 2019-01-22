51c9af472f17c41029b68858bee82e1e369a8fcb
electricsheep718 <nyybaseball718@gmail.com>
2019-01-22 15:46:14 -0500

    update

--- Scripts/Sami Test Cases/ICRC - External - Registration - Date Picker Test/Script1548093460166.groovy
+++ Scripts/Sami Test Cases/ICRC - External - Registration - Date Picker Test/Script1548093460166.groovy
@@ -13,3 +13,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.acceptAlert()
+
+WebUI.acceptAlert()
+
