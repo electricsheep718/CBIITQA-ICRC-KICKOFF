5733bb187446850db926fe2956017b40f38ef1a9
kaurs3 <kaurs3@NCI-02096754-L.nci.nih.gov>
2019-01-23 14:07:05 -0500

    NEw suite

---
+++ Test Suites/Test Suite Collections/Login Test suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Test suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3af18a90-87b3-49a3-ad25-dc61bc2bbf22</testSuiteGuid>
+</TestSuiteEntity>
