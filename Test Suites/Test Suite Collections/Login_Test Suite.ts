1884040d7d2ca31eb440bb523734f1c58970b72f
kaurs3 <kaurs3@NCI-02096754-L.nci.nih.gov>
2019-01-23 14:26:35 -0500

    Updated Suite

---
+++ Test Suites/Test Suite Collections/Login_Test Suite.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8992032b-8cde-4480-946c-53f86cbde51b</testSuiteGuid>
+   <testCaseLink>
+      <guid>1be168b4-c742-4344-9647-e08152035737</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Simran Test Cases/ICRC-External-Registration- New</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>ed526138-2723-4ca2-b82f-cbef302d7680</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>1-1</value>
+         </iterationEntity>
+         <testDataId>Data Files/DataOne-Registration-Simran</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>ed526138-2723-4ca2-b82f-cbef302d7680</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>FirstName</value>
+         <variableId>70ef550f-77f3-43ce-9ed0-931f1915d6c7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>ed526138-2723-4ca2-b82f-cbef302d7680</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>LastName</value>
+         <variableId>42db4a5e-83d6-4bbb-9052-a34ad9d95ccf</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>ed526138-2723-4ca2-b82f-cbef302d7680</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>EmailAddress</value>
+         <variableId>ea93b3a7-2c96-4d6b-87ee-ea304af9b05f</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9709d85a-ec1c-4a40-9489-25b09924acc8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Simran Test Cases/ICRC External - Login - Locked Out Email - Simran</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e045a3cc-c0aa-4c31-9bef-a1866ea91c39</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Simran Test Cases/ICRC External - Login - Negative LoginTest - Simran</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>394aeeea-abe6-4487-b681-99f349ad3f1b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Simran Test Cases/ICRC External-InvalidEmailAdd-Simran</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
