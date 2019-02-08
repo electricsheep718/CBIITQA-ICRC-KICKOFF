0c709613e6ac834cf1ddf6486931bc05161d3a30
electricsheep718 <nyybaseball718@gmail.com>
2019-02-08 11:47:39 -0500

    Revert "Setting up the project"
    This reverts commit 151fe4db34687137dd63b6526648026690663259.

--- bin/Test Suites/Test Suite Collections/Register And Submit.ts
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<TestSuiteCollectionEntity>
-   <description></description>
-   <name>Register And Submit</name>
-   <tag></tag>
-   <executionMode>SEQUENTIAL</executionMode>
-   <maxConcurrentInstances>8</maxConcurrentInstances>
-   <testSuiteRunConfigurations/>
-</TestSuiteCollectionEntity>
